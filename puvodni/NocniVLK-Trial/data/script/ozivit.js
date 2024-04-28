31452e5e2d4d567681dcdc2160c3156ad0c3ede3
Michal Raus <raus.michal@email.cz>
2024-04-28 18:05:48 +0200

    Přechod z ES5 na ES6

--- puvodni/NocniVLK-Trial/data/script/ozivit.js
+++ puvodni/NocniVLK-Trial/data/script/ozivit.js
@@ -1,29 +1,24 @@
-﻿var uloz={osoba_kopie:{},z_den:"",cas_T:"",intr:"",ok:null,klice:["osoba","cas_p","obchuz","cas_to","o15","o30","o60","o120","interval","vlk_zas","alarm_v","alarm_zv"],max_obnova_ms:3600000,v_obchuzce:false,TIME:1000};
-uloz.casovac;
-
-uloz.p={pruvodce:null,autorun:null,vlk:null,ochrany:null,kresly:null}; /* objekt shromažďuje data o připravenosti jednotlivých js knihoven, aby po jejich připravenosti bylo možné oživení */
-
-uloz.a=function(){
+﻿const uloz={osoba_kopie:{},z_den:"",cas_T:"",intr:"",ok:null,klice:["osoba","cas_p","obchuz","cas_to","o15","o30","o60","o120","interval","vlk_zas","alarm_v","alarm_zv"],max_obnova_ms:3600000,v_obchuzce:false,TIME:1000,casovac:null,
+p:{pruvodce:null,autorun:null,vlk:null,ochrany:null,kresly:null}, /* objekt shromažďuje data o připravenosti jednotlivých js knihoven, aby po jejich připravenosti bylo možné oživení */
+a(){
 /* funkce slouží k aktivaci - posouzení, zda je možno použít localstorage */
 
 if(this.ok==null)
 {
-var retezec=location.search.slice(1); /* odebere Otazník z řetězce - aby mohlo dojít ke konverci JSON */
+let retezec=location.search.slice(1); /* odebere Otazník z řetězce - aby mohlo dojít ke konverci JSON */
+let pole="";
 try
 {
-var pole=JSON.parse(retezec);
+pole=JSON.parse(retezec);
 }
 catch(e)
 {
 return; /* něco se pokazilo a nepodařilo se dostat pomocí JSON z řetězce pole - funkce bude ukončena */
 }
 this.ok=pole[1]; /* první položka v poly je výsledek testu, zda funguje Local storage - viz start.js funkce over */
-}};
-
-uloz.uloz=function(klic,data){
+}},
+uloz(klic,data){
 /* funkce zajišťuje ukládání dat na local storage */
-
-/* alert(klic+"   "+data); */ 
 
 /* podmínky funkčnosti localstorage */
 if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local storage */ }
@@ -33,9 +28,8 @@ if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití loca
 localStorage.removeItem(klic); /* nejprve provede smazání dat pod klíčem */
 localStorage.setItem(klic,data); /* provede uložení dat na localStorage [klíč,data k uložení] */
 
-};
-
-uloz.nacti=function(klic){
+},
+nacti(klic){
 /* funkce zajišťuje ukládání dat na local storage */
 
 /* podmínky funkčnosti localstorage */
@@ -43,7 +37,7 @@ if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local stora
 if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití local storage - provede return */ }
 /* KONEC podmínky funkčnosti localstorage */
 
-var data=localStorage.getItem(klic); /* provede nahrání dat z localStorage [klíč] */
+let data=localStorage.getItem(klic); /* provede nahrání dat z localStorage [klíč] */
 if(data==null)
 {
 data=""; /* pokud jsou data null - jsou data "" */
@@ -51,10 +45,8 @@ data=""; /* pokud jsou data null - jsou data "" */
 
 return data;
 
-};
-
-
-uloz.smaz=function(klic){
+},
+smaz(klic){
 /* funkce zajišťuje mazání dat z local storage */
 
 /* podmínky funkčnosti localstorage */
@@ -63,9 +55,8 @@ if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití loca
 /* KONEC podmínky funkčnosti localstorage */
 
 localStorage.removeItem(klic); /* smazání dat pod klíčem */
-};
-
-uloz.osoba=function(){
+},
+osoba(){
 /* funkce slouží k ukládání dat objektu OSOBA, který shromažďuje data zvolenmá uživatelem v pruvodce.js */
 
 /* podmínky funkčnosti localstorage */
@@ -73,12 +64,11 @@ if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local stora
 if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití local storage - provede return */ }
 /* KONEC podmínky funkčnosti localstorage */
 
-var data=osoba; /* Objekt v pruvodce.js */
-var konverce=JSON.stringify(data); /* provede konverzi, která je následně připravena k použití */
+const data=osoba; /* Objekt v pruvodce.js */
+let konverce=JSON.stringify(data); /* provede konverzi, která je následně připravena k použití */
 this.uloz(this.klice[0],konverce); /* pošle data funkci, která se postará o uložení na localstorage */
-};
-
-uloz.s_obch=function(){
+},
+s_obch(){
 
 /* podmínky funkčnosti localstorage */
 if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local storage */ }
@@ -90,9 +80,8 @@ localStorage.removeItem(this.klice[4]); /* smaže obchůzky do 15minut */
 localStorage.removeItem(this.klice[5]); /* smaže obchůzky do 30minut */
 localStorage.removeItem(this.klice[6]); /* smaže obchůzky do 60minut */
 localStorage.removeItem(this.klice[7]); /* smaže obchůzky do 120minut */
-};
-
-uloz.u_obch=function(){
+},
+u_obch(){
 /* funkce uloží všechny obchůzky z příslušných formulářů */
 
 /* podmínky funkčnosti localstorage */
@@ -100,19 +89,14 @@ if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local stora
 if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití local storage - provede return */ }
 /* KONEC podmínky funkčnosti localstorage */
 
-
-var df15=document.getElementById(obch.id_f[0]).value; /* data formulář zápisu obchůzek do 15 minut - pole obch.id_f je v vlk.js */
-var df30=document.getElementById(obch.id_f[1]).value;
-var df60=document.getElementById(obch.id_f[2]).value;
-var df120=document.getElementById(obch.id_f[3]).value;
+const [df15,df30,df60,df120]=[document.getElementById(obch.id_f[0]).value,document.getElementById(obch.id_f[1]).value,document.getElementById(obch.id_f[2]).value,document.getElementById(obch.id_f[3]).value]; /* data formulář zápisu obchůzek do 15,30,60,120 minut - pole obch.id_f je v vlk.js */
 
 this.uloz(this.klice[4],df15); /* uloží na local storage obchůzky do 15minut v oziv.js */
 this.uloz(this.klice[5],df30); /* uloží na local storage obchůzky do 30minut v oziv.js */
 this.uloz(this.klice[6],df60);
 this.uloz(this.klice[7],df120);
-};
-
-uloz.o_obch=function(){
+},
+o_obch(){
 /* funkce oživí obchůzky do příslušných formulářů */
 
 /* podmínky funkčnosti localstorage */
@@ -120,29 +104,18 @@ if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local stora
 if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití local storage - provede return */ }
 /* KONEC podmínky funkčnosti localstorage */
 
+let o15=this.nacti(this.klice[4]); /* načte obchůzky do 15minut z localstorage */
+let o30=this.nacti(this.klice[5]); /* načte obchůzky do 30minut z localstorage */
+let o60=this.nacti(this.klice[6]); /* načte obchůzky do 60minut z localstorage */
+let o120=this.nacti(this.klice[7]); /* načte obchůzky do 120minut z localstorage */
 
-var f15=document.getElementById(obch.id_f[0]); /* formulář zápisu obchůzek do 15 minut - pole obch.id_f je v vlk.js */
-var f30=document.getElementById(obch.id_f[1]);
-var f60=document.getElementById(obch.id_f[2]);
-var f120=document.getElementById(obch.id_f[3]);
+document.getElementById(obch.id_f[0]).value=o15; /* zapíše obchůzky 15min do formuláře */
+document.getElementById(obch.id_f[1]).value=o30; /* zapíše obchůzky 30min do formuláře */
+document.getElementById(obch.id_f[2]).value=o60;  /* zapíše obchůzky 60min do formuláře */
+document.getElementById(obch.id_f[3]).value=o120;  /* zapíše obchůzky 120min do formuláře */
 
-var o15=this.nacti(this.klice[4]); /* načte obchůzky do 15minut z localstorage */
-var o30=this.nacti(this.klice[5]); /* načte obchůzky do 30minut z localstorage */
-var o60=this.nacti(this.klice[6]); /* načte obchůzky do 60minut z localstorage */
-var o120=this.nacti(this.klice[7]); /* načte obchůzky do 120minut z localstorage */
-
-
-f15.value=o15; /* zapíše obchůzky do formuláře */
-f30.value=o30;
-f60.value=o60;
-f120.value=o120;
-
-};
-
-
-
-
-uloz.o_Tout=function(){
+},
+o_Tout(){
 /* FUNKCE oživí počátek času počítání TIME OUTU */
 
 /* podmínky funkčnosti localstorage */
@@ -151,23 +124,21 @@ if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití loca
 /* KONEC podmínky funkčnosti localstorage */
 
 
-var cas_T=this.nacti(this.klice[3]);  /* načte čas zahájení timeoutu z localstorage v sekundách od roku 1970 */
+let cas_T_n=this.nacti(this.klice[3]);  /* načte čas zahájení timeoutu z localstorage v sekundách od roku 1970 */
 
 
-
-if(cas_T=="")
+if(cas_T_n=="")
 {
 this.cas_T=null; /* pokud nebude čas timeautu bude 0,0,0 */
 }
 else
 {
-cas_T=parseInt(cas_T);
-this.cas_T=cas_T;
+cas_T_n=parseInt(cas_T_n);
+this.cas_T=cas_T_n;
 }
 
-};
-
-uloz.o_osoba=function(){
+},
+o_osoba(){
 /* načte hodnoty pro úpravu objektu osoba - v pruvodce.js */
 
 
@@ -176,7 +147,7 @@ if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local stora
 if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití local storage - provede return */ }
 /* KONEC podmínky funkčnosti localstorage */
 
-var data_osoba=this.nacti(this.klice[0]); /* načte objekt osoba z localstorage */
+let data_osoba=this.nacti(this.klice[0]); /* načte objekt osoba z localstorage */
 if(data_osoba==""){this.osoba_kopie=""; return;} /* pokud nebudou načtena žádná data - bude return */
 
 try
@@ -185,14 +156,12 @@ this.osoba_kopie=JSON.parse(data_osoba); /* z dat uživatele udělá objekt osob
 }
 catch(e)
 {
-this.osoba_kopie="";
-alert("data Osoby jsou poškozena!");
+this.osoba_kopie=""; /* načtené data osoby jsou poškozená */
 return;
 }
 
-};
-
-uloz.o_cas_P=function(){
+},
+o_cas_P(){
 /* načtení hodnot počátku počátíná času do intervalu */
 
 /* podmínky funkčnosti localstorage */
@@ -200,7 +169,7 @@ if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local stora
 if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití local storage - provede return */ }
 /* KONEC podmínky funkčnosti localstorage */
 
-var cas_P=localStorage.getItem(this.klice[1]);  /* načte čas zahájení odpočtu z localstorage */
+let cas_P=localStorage.getItem(this.klice[1]);  /* načte čas zahájení odpočtu z localstorage */
 
 if(cas_P==null){
 this.z_den="";
@@ -208,9 +177,8 @@ return; /* pokud nebudou načtena žádná data - bude return */
 }
 
 this.z_den=parseInt(cas_P); /* čas v milisekundách od roku cca 1970  */
-};
-
-uloz.o_v_obchuz=function(){
+},
+o_v_obchuz(){
 /* načte data,zda nedochází k oživení v obchůzce */
 
 /* podmínky funkčnosti localstorage */
@@ -218,26 +186,20 @@ if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local stora
 if(this.ok!=true){return; /* pokud pro zařízení nebude možné použití local storage - provede return */ }
 /* KONEC podmínky funkčnosti localstorage */
 
-var v_obchuzce=this.nacti(this.klice[2]);  /* načte zda nedošlo k oživení v obchůzce */
+let v_obch=this.nacti(this.klice[2]);  /* načte zda nedošlo k oživení v obchůzce */
 
-if(v_obchuzce=="true")
+if(v_obch=="true")
 { /* pokud byla obchůzka aktivní */
 this.v_obchuzce=true;
 }
 else
 { /* pokud nebyla obchůzka aktivní */
 this.v_obchuzce=false;
-}
-
-
-};
-
-uloz.dead_time=function(){
+}},
+dead_time(){
 /* KONTROLA - ABY NEDOŠLO K OŽIVENÍ VLKA po více jak 60MINUTÁCH od plánovaného TIMEOUTU */
-var datum=new Date();
-var akt_ms=datum.getTime(); /* čas v milisekundách od roku cca 1970 */
-
-var interval=this.intr*1000; /* délka intervalu v sekundách se přepíše na milisekundy */
+let akt_ms=Date.now(); /* vrátí počet milisekund od nulového data (1. ledna 1970 00:00:00 UTC) */
+let interval=this.intr*1000; /* délka intervalu v sekundách se přepíše na milisekundy */
 
 if(akt_ms<(this.z_den+this.max_obnova_ms+interval))
 {
@@ -249,9 +211,8 @@ else
 return false; /* pokud budou data k obnově starší než this.max_obnova_ms - ČAS V MILISEKUNDÁCH - bude false */
 }
 /* konec - KONTROLA - ABY NEDOŠLO K OŽIVENÍ VLKA po více jak 120MINUTÁCH */
-};
-
-uloz.klonKOPII=function(){
+},
+klonKOPII(){
 /* přepis globálních proměnných z klonů a jejich vyčištění */
 
 osoba=this.osoba_kopie; /* provede se přepis Doplněné kopie Objektu osoba načtených z localStorage - v oziv.js */
@@ -264,21 +225,20 @@ this.z_den=""; /* vymažou se dočasně uložená data */
 obch.intr=this.intr; /* načte ionterval do obchůzky z klonu */
 this.intr="";
 
-};
-
-uloz.o_zvuk=function(){
+},
+o_zvuk(){
 /* funkce oživí Volbu zvuku alarmu uživatelem + volbu zda chce Postupně zesilovat zvuk */
 
-var volba=this.nacti(this.klice[10]); /* načte volbu zvuku uživatele uloženou na LocalStorage */
+let volba=this.nacti(this.klice[10]); /* načte volbu zvuku uživatele uloženou na LocalStorage */
 
-var zesilovani=this.nacti(this.klice[11]); /* načte volbu uživatele, zda chce postupně zvyšovat zvuk */
+let zesilovani=this.nacti(this.klice[11]); /* načte volbu uživatele, zda chce postupně zvyšovat zvuk */
 
 if(volba!="")
 {
 /* pokud byla načtena nějáká volba */
 volba=parseInt(volba); /* převede textový řetězec na číslo */
 zvuk.cislo=volba; /* provede změnu volby v objektu zvuk - ve vlk.js */
-var typ=zvuk.cislo-1; /* ubere číslu 1, aby odpovídalo začátku pole this.alarm - ve vlk.js  */
+let typ=zvuk.cislo-1; /* ubere číslu 1, aby odpovídalo začátku pole this.alarm - ve vlk.js  */
 zvuk.cesta=zvuk.alarm[typ]; /* změní cestu podle výběru - ve vlk.js  */
 zvuk.nahraj(); /* nahraje mp3 do paměti - ve vlk.js  */
 }
@@ -298,9 +258,8 @@ else if(zesilovani=="false")
 zvuk.zesilovat=false; /* nastaví proměnnou na Zakázat postupné zesilování - ve vlk.js */
 document.getElementById(p_nas.id_nas[9]).checked=false; /* odstraní Zatržení na Chckeboxu Postupně zesilovat alarm - id v autorun.js */
 }}
-};
-
-uloz.oziv=function(tlacitkem){
+},
+oziv(tlacitkem){
 
 /* podmínky funkčnosti localstorage */
 if(this.ok==null){ this.a(); /* aktivace - posouzení použitelnosti Local storage */ }
@@ -335,7 +294,7 @@ this.intr=parseInt(this.intr); /* přebede text na číslo */
 
 
  /* KONTROLA - ABY NEDOŠLO K OŽIVENÍ VLKA po delším čase */
-var test=this.dead_time();
+const test=this.dead_time();
 if(!test)
 {
 return; /* pokud bude test false - obnova nebude */
@@ -348,7 +307,7 @@ this.o_v_obchuz(); /* zjistí,zda nedošlo není oživení v obchůzce */
 if(!tlacitkem)
 {
 /* pokud nedošlo k oživení nočního VLKa Tlačítkem Oživit v hlavním kontejneru */
-var byl_vlk_zastaven=this.nacti(this.klice[9]); /* načte hodnotu z local storage */
+const byl_vlk_zastaven=this.nacti(this.klice[9]); /* načte hodnotu z local storage */
 if(byl_vlk_zastaven=="true")
 {
 /* Pokud byl noční VLK zastaven */
@@ -368,9 +327,8 @@ this.o_obch(); /* funkce oživí obchůzky do příslušných formulářů */
 vlk.ozivit(); /* spustí oživovací procesy Nočního VLKA - ve vlk.js */
 }
 
-};
-
-uloz.akce=function(){
+},
+akce(){
 
 if(this.p.pruvodce==true&&this.p.autorun==true&&this.p.vlk==true&&this.p.ochrany==true&&this.p.kresly==true)
 {
@@ -395,7 +353,7 @@ else
 this.casovac=setInterval(this.akce.bind(this),this.TIME);
 }
 
-};
+}};
 
 
 uloz.akce();
